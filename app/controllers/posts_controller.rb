ca2bf6063f4ef479a2b0f4bf3f36e7fe15e4a625
cr1tterp0wer <capodacac@gmail.com>
2017-08-29 17:32:58 -0700

    has-many association polymorphic done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  
+   before_action :require_logged_in!, only:[:new,:create]
   def show
     @post = Post.find(params[:id])
   end
@@ -9,15 +9,15 @@ class PostsController < ApplicationController
   end
 
   def create
+    sub_ids = params[:sub][:id].map{|el|el.to_i }
     @post = Post.new(title: post_params[:title], 
-                    url: post_params[:url])
-    
-   debugger
-    #get sub
-    #get sub.post_ids
-    #push new post id into post_ids array
-    #set sub.post_ids = array_post_ids
-   
+                    url: post_params[:url],
+                    sub_ids: sub_ids)
+    if @post.save
+       redirect_to subs_url
+    else
+      flash[:errors]= @post.errors.full_messages
+    end
   end
 
   private
